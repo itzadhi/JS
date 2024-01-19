7506b2e9684382b26d9482a713a7491a8acc8e37
adhit7 <adhithyanalan@gmail.com>
2024-01-19 20:53:47 +0530

    update changes

--- callback/script.js
+++ callback/script.js
@@ -1,8 +1,11 @@
 let timer = 10;
-let display = document.querySelector('.display');
+let display = document.getElementById('display');
+console.log('hello timeout');
 setTimeout(() => {
+  console.log('1');
   display.innerText = timer--;
   setTimeout(() => {
+    console.log('2');
     display.innerText = timer--;
     setTimeout(() => {
       display.innerText = timer--;

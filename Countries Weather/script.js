971439a01a7883385954a5a317b59dc6b9009570
adhit7 <adhithyanalan@gmail.com>
2024-01-25 19:55:10 +0530

    update changes

--- Countries Weather/script.js
+++ Countries Weather/script.js
@@ -1,14 +1,17 @@
-fetch('https://restcountries.com/v3.1/all')
-  .then((response) => response.json())
-  .then((data) => {
-    return process(data);
-  });
+const restCountries = async () => {
+  const response = await fetch('https://restcountries.com/v3.1/all');
+  const data = await response.json();
+  return displayData(data);
+};
 
-let temperature = null;
-
-function process(data) {
+function displayData(data) {
   let mainContainer = document.createElement('div');
   mainContainer.classList.add('container');
+
+  let header = document.createElement('h1');
+  header.id = 'title';
+  header.classList.add('text-center', 'fs-2');
+  header.innerText = 'Rest Countries with Weather Details';
 
   let container = document.createElement('div');
   container.id = 'card';
@@ -35,7 +38,7 @@ function process(data) {
         </div>
         <div class="card-body">
           <img src=${data[i].flags.png} class="card-img-top my-brand-image">
-          <div class="card-text d-flex flex-column ">
+          <div id=${data[i].name.common} class="card-text d-flex flex-column ">
             <p class="card-subtitle text-center text-light pb-2 mt-2">Capital :${data[i].capital}</p>
             <p class="card-subtitle text-center text-light pb-2">Region :${data[i].region}</p>
             <p class="card-subtitle text-center text-light pb-2">Country Code :${data[i].cca3}</p>
@@ -47,82 +50,20 @@ function process(data) {
     `;
 
     container.append(cardWrapper);
-
-    // let cardHeader = document.createElement('div');
-    // cardHeader.classList.add(
-    //   'card-title',
-    //   'text-center',
-    //   'bg-black',
-    //   'fs-5',
-    //   'p-3',
-    //   'text-truncate',
-    //   'text-light'
-    // );
-
-    // cardHeader.classList.add('card-header');
-
-    // let cardHeaderTitle = document.createElement('h4');
-    // cardHeaderTitle.classList.add(
-    //   'card-title',
-    //   'd-flex',
-    //   'justify-content-center'
-    // );
-
-    // cardHeaderTitle.innerHTML = data[i].name.common;
-
-    // cardHeader.append(cardHeaderTitle);
-
-    // let cardImage = document.createElement('img');
-    // cardImage.classList.add('card-img', 'p-2', 'my-brand-image');
-    // cardImage.src = data[i].flags.png;
-
-    // let capital = document.createElement('div');
-    // capital.classList.add('card-subtitle', 'text-center', 'text-light', 'pb-1');
-    // capital.innerHTML = `Capital : ${data[i].capital}`;
-
-    // let region = document.createElement('div');
-    // region.classList.add(
-    //   'card-body',
-    //   'card-text',
-    //   'card-subtitle',
-    //   'text-center',
-    //   'text-light',
-    //   'pb-1'
-    // );
-    // region.innerHTML = `Region : ${data[i].region}`;
-
-    // let countryCode = document.createElement('div');
-    // countryCode.classList.add(
-    //   'card-text',
-    //   'card-subtitle',
-    //   'text-center',
-    //   'text-light',
-    //   'pb-1'
-    // );
-    // countryCode.innerHTML = `Country Code  : ${data[i].cca3}`;
-
-    // let button = document.createElement('btn');
-    // button.classList.add('btn', 'btn-primary', 'align-self-center', 'mb-4');
-    // button.innerText = 'Click for Weather';
-
-    // card.append(cardHeader, cardImage, capital, region, countryCode, button);
-
-    // cardWrapper.append(card);
   }
-  mainContainer.append(container);
+  mainContainer.append(header, container);
   document.body.append(mainContainer);
 }
 
-if (temperature != null) {
-  let btn = document.getElementsByClassName('btn');
-  console.log('11', btn);
-}
-
-async function weatherApi(lat, lon) {
+const weatherApi = async (lat, lon) => {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9ae271f72ce5677f1897de2c25eb5998`
   );
   const data = await response.json();
-  alert('Temperature: ' + data.main.temp);
+  alert(
+    'Temperature: ' + data.main.temp + ' and Humidity: ' + data.main.humidity
+  );
   return data.main.temp;
-}
+};
+
+restCountries();
